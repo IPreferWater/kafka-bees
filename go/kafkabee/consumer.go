22b7baa6b84d5e6b2286b31b832a0d60dae0a188
IPreferWater <ipreferwater2@gmail.com>
2022-01-16 12:50:40 +0100

    feat(dev): add consumer for topic "detected"

---
+++ go/kafkabee/consumer.go
@@ -0,0 +1,82 @@
+package kafkabee
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/riferrei/srclient"
+)
+
+func InitConsumer() {
+
+	conf := getConfig()
+
+	kafkaConfigMap := &kafka.ConfigMap{
+		"bootstrap.servers": conf.kafkaUrl,
+		"security.protocol": conf.securityProtocol,
+		"group.id":          "consumer-detected",
+		"auto.offset.reset": "latest",
+	}
+
+	if conf.securityProtocol != "PLAINTEXT" {
+		kafkaConfigMap.SetKey("ssl.ca.location", conf.pathCaPem)
+		kafkaConfigMap.SetKey("ssl.certificate.location", conf.pathServiceCert)
+		kafkaConfigMap.SetKey("ssl.key.location", conf.pathServiceKey)
+	}
+
+	c, err := kafka.NewConsumer(kafkaConfigMap)
+	if err != nil {
+		panic(fmt.Sprintf("can't create consumer %s", err))
+	}
+
+	err = c.SubscribeTopics([]string{conf.topic}, nil)
+
+	schemaRegistryClient := srclient.CreateSchemaRegistryClient(conf.schemaRegistryUrl)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err != nil {
+			panic(fmt.Sprintf("error readin msg %s\n", err))
+		}
+
+		//value
+		schemaID := binary.BigEndian.Uint32(msg.Value[1:5])
+
+		schema, err := schemaRegistryClient.GetSchema(int(schemaID))
+
+		if err != nil {
+			panic(fmt.Sprintf("Error getting the schema with id '%d' %s", schemaID, err))
+		}
+
+		native, _, _ := schema.Codec().NativeFromBinary(msg.Value[5:])
+		value, _ := schema.Codec().TextualFromNative(nil, native)
+
+		detected := DataValue{}
+		if err := json.Unmarshal(value, &detected); err != nil {
+			panic(fmt.Sprintf("error unmarshall the string %s err => %s\n", string(value), err))
+		}
+
+		//key
+
+		schemaIDKey := binary.BigEndian.Uint32(msg.Key[1:5])
+
+		schemaKey, err := schemaRegistryClient.GetSchema(int(schemaIDKey))
+
+		if err != nil {
+			panic(fmt.Sprintf("Error getting the schema key with id '%d' %s", schemaIDKey, err))
+		}
+
+		nativeKey, _, _ := schemaKey.Codec().NativeFromBinary(msg.Key[5:])
+		key, _ := schemaKey.Codec().TextualFromNative(nil, nativeKey)
+
+		detectedKey := DataKey{}
+		if err := json.Unmarshal(key, &detectedKey); err != nil {
+			panic(fmt.Sprintf("error unmarshall the string %s err => %s\n", string(key), err))
+		}
+
+		fmt.Printf("key : %#v\n value : %#v\n***\n", detectedKey, detected)
+	}
+
+}
