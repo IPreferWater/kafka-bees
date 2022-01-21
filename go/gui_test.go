ed7ea5b0bdd7cd5997e95a5908dd495346f25f23
IPreferWater <ipreferwater2@gmail.com>
2022-01-21 21:39:24 +0100

    feat(dev): add test for GetCloserFromHive

--- go/gui_test.go
+++ go/gui_test.go
@@ -1,24 +1,50 @@
 package main
 
 import (
+	"math"
 	"testing"
 )
 
-//TODO we should get the distance instead to test every situations
 func TestGetCloserFromHive(t *testing.T) {
 	hiveEntryX, hiveEntryY := float64(110), float64(140)
-	xStart, yStart := float64(100), float64(200)
-
-	insect := Insect{
-		position: coordinate{
-			x: float64(xStart),
-			y: float64(yStart),
+	positionsToTest := [4]coordinate{
+		{
+			x: 80,
+			y: 120,
+		},
+		{
+			x: 145,
+			y: 112,
+		},
+		{
+			x: 105,
+			y: 160,
+		},
+		{
+			x: 180,
+			y: 180,
 		},
 	}
-	getCloserFromHive(&insect, hiveEntryX, hiveEntryY)
 
-	if insect.position.x <= xStart || insect.position.y <= yStart {
-		t.Errorf("x & y should increased but xStart %f current x %f & yStart %f current y %f\n", xStart, insect.position.x, yStart, insect.position.y)
+	
+	for _, coordinateToTest := range positionsToTest {
+		insect := Insect{
+			position: coordinateToTest,
+		}
+		getCloserFromHive(&insect, hiveEntryX, hiveEntryY)
+
+		distanceStart := getDistanceBeetweenTwoPoints(coordinateToTest.x, coordinateToTest.y, hiveEntryX, hiveEntryY)
+		distanceGot := getDistanceBeetweenTwoPoints(insect.position.x, insect.position.y, hiveEntryX, hiveEntryY)
+
+		if distanceStart < distanceGot {
+			t.Errorf("the insect was %f pixels away and now %f, he should be closer\n", distanceStart, distanceGot)
+		}
 	}
 
+}
+
+func getDistanceBeetweenTwoPoints(x1, y1, x2, y2 float64) float64 {
+	//d(A,B)=√(x2−x1)2+(y2−y1)2
+	toSquare := math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2)
+	return math.Sqrt(toSquare)
 }
